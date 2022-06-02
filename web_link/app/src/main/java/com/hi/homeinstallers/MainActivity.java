2fecb617318dbb68597a400f9491b0c74c2e700b
mimoc <mihaella0293@yahoo.com>
2022-06-02 17:29:15 +0300

    added image picker

--- web_link/app/src/main/java/com/hi/homeinstallers/MainActivity.java
+++ web_link/app/src/main/java/com/hi/homeinstallers/MainActivity.java
@@ -1,18 +1,30 @@
 package com.hi.homeinstallers;
 
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.Intent;
+import android.net.Uri;
 import android.net.http.SslError;
 import android.os.Bundle;
 import android.webkit.CookieManager;
 import android.webkit.SslErrorHandler;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private static final String URL_HOMEINSTALLERS = "https://app.homeinstallers.co.uk";
 
     private WebView webViewurl;
+
+    private ValueCallback<Uri[]> uploadMessageAboveL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +52,69 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
+        webViewurl.setWebChromeClient(new WebChromeClient() {
+
+
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+            }
+
+            //For Android API >= 21 (5.0 OS)
+            @Override
+            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams) {
+                uploadMessageAboveL = filePathCallback;
+                openImageChooserActivity();
+                return true;
+            }
+
+        });
+
         webViewurl.loadUrl(URL_HOMEINSTALLERS);
+    }
+
+    private void openImageChooserActivity() {
+        Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+        i.addCategory(Intent.CATEGORY_OPENABLE);
+        i.setType("image/*");
+        attachmentPickerActivity.launch(i);
+    }
+
+    // You can do the assignment inside onAttach or onCreate, i.e, before the activity is displayed
+    ActivityResultLauncher<Intent> attachmentPickerActivity = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // There are no request codes
+                        Intent data = result.getData();
+                        onActivityResultAboveL(result.getResultCode(), data);
+                    }
+                }
+            });
+
+
+    private void onActivityResultAboveL(int resultCode, Intent intent) {
+        Uri[] results = null;
+        if (resultCode == Activity.RESULT_OK) {
+            if (intent != null) {
+                String dataString = intent.getDataString();
+                ClipData clipData = intent.getClipData();
+                if (clipData != null) {
+                    results = new Uri[clipData.getItemCount()];
+                    for (int i = 0; i < clipData.getItemCount(); i++) {
+                        ClipData.Item item = clipData.getItemAt(i);
+                        results[i] = item.getUri();
+                    }
+                }
+                if (dataString != null)
+                    results = new Uri[]{Uri.parse(dataString)};
+            }
+        }
+        uploadMessageAboveL.onReceiveValue(results);
+        uploadMessageAboveL = null;
     }
 
 }
